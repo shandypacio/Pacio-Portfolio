191e9b6aa51b2a3adf29cfbd4b84515d96f77ee0
shandypacio <shandygale.pacio@tup.edu.ph>
2024-11-19 18:38:54 +0800

    polishing some code in index, added css to navigation to be fixed position, and script also for navbar

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('bg-white', 'shadow');
+  } else {
+    navbar.classList.remove('bg-white', 'shadow');
+  }
+});
