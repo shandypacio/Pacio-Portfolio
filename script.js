59bba6098394c229c14f56e13f6762953c3f779f
shandypacio <shandygale.pacio@tup.edu.ph>
2024-11-20 21:15:33 +0800

    animation

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ window.addEventListener('scroll', function () {
     navbar.classList.remove('bg-white', 'shadow');
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".nav-link");
+
+  navLinks.forEach((link) => {
+      link.addEventListener("click", (e) => {
+          e.preventDefault();
+          
+
+          const targetId = link.getAttribute("href").substring(1); 
+          const targetSection = document.getElementById(targetId);
+          
+          if (targetSection) {
+              targetSection.classList.remove("animated-section");
+
+              setTimeout(() => {
+                  targetSection.classList.add("animated-section");
+              }, 50);
+
+              targetSection.scrollIntoView({ behavior: "smooth", block: "start" });
+          }
+      });
+  });
+});
